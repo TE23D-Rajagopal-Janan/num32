b2f67c6d344198d6da821df282410a5edce76bda
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-09-13 10:11:27 +0200

    yugouyg

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿//string name = "Janan";
+//bool isAlive = name == "Janan";
+
+//int hp = 100; 
+
+//hp = hp -10 
+
+
+//console.WriteLine(hp)
+
+//if (hp >= 0)
+//{
+// Console.WriteLine ("Still Alive");
+//}
+
+Console.WriteLine("Vilken Klass går jag i ");
+Console.WriteLine("1)Te23C   2)TE23B  3)TE23D");
+string ans1 = Console.ReadLine();
+if (ans1 == "1")
+{
+
+Console.WriteLine("Fel Svar");
+
+if (ans1 == "2")
+ {
+    Console.WriteLine("Fel Svar");
+ }
+ else if (ans1 == "3")
+ {
+    Console.WriteLine("Korrekt");
+ }
+}
+
+
+Console.ReadLine();
+
