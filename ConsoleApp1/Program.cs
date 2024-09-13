f3f323a46b8e3562b1edf3e2c876357314e41a88
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-09-13 10:55:14 +0200

    kjafkjwa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,36 +1,75 @@
-﻿//string name = "Janan";
-//bool isAlive = name == "Janan";
-
-//int hp = 100; 
-
-//hp = hp -10 
-
-
-//console.WriteLine(hp)
-
-//if (hp >= 0)
-//{
-// Console.WriteLine ("Still Alive");
-//}
-
+﻿int Poäng = 0; 
 Console.WriteLine("Vilken Klass går jag i ");
 Console.WriteLine("1)Te23C   2)TE23B  3)TE23D");
 string ans1 = Console.ReadLine();
 if (ans1 == "1")
 {
-
 Console.WriteLine("Fel Svar");
-
-if (ans1 == "2")
+}
+else if (ans1 == "2")
  {
     Console.WriteLine("Fel Svar");
  }
  else if (ans1 == "3")
  {
     Console.WriteLine("Korrekt");
+    Poäng = Poäng +10;
  }
+
+Console.WriteLine("Du har"); Console.WriteLine(Poäng);Console.WriteLine("Poäng");
+Console.WriteLine("Hur gammal är Jag");
+Console.WriteLine("1)16 2)17 3)18");
+string ans2 = Console.ReadLine();
+if (ans2 == "1")
+{
+   Console.WriteLine("fel svar"); 
+}
+else if (ans2 == "2")
+{
+   Console.WriteLine("Rätt svar");
+   Poäng = Poäng +10; 
+} 
+else if (ans2 == "3")
+{
+   Console.WriteLine("Fel svar");
 }
 
+Console.WriteLine("Du har"); Console.WriteLine(Poäng);Console.WriteLine("Poäng");
+Console.WriteLine("När är jag född");
+Console.WriteLine("1)12 Maj 2)12 September 3)12 Feburary");
+string ans3 = Console.ReadLine();
+if (ans3 == "1")
+{
+   Console.WriteLine("Fel svar");
+}
+else if (ans3 == "2")
+{
+   Console.WriteLine("Fel svar");
+}
+else if(ans3 == "3")
+{
+Console.WriteLine("Rätt svar");
+Console.WriteLine("Bra jobbat");
+Poäng = Poäng +10;
+}
+Console.WriteLine("Så här bra gjorde du");
+Console.WriteLine("Du fick totalt"); Console.WriteLine(Poäng);Console.WriteLine("Poäng");
+if (Poäng == 30)
+{
+ Console.WriteLine ("Alla rätt Bra jobbat");
+}
+else if (Poäng == 20)
+{
+   Console.WriteLine ("A du är ändå helt okej");
+}
+else if (Poäng == 10) 
+{
+Console.WriteLine("Katastrofalt");
+}
+else if (Poäng == 0) 
+{
+Console.WriteLine("Katastrofalt");
+}
 
 Console.ReadLine();
 
