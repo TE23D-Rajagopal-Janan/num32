730fe19979be6f74dbe5e1261471d3b4eddba8f0
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-09-16 09:05:50 +0200

    fwfw

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,7 +16,7 @@ else if (ans1 == "2")
     Poäng = Poäng +10;
  }
 
-Console.WriteLine("Du har"); Console.WriteLine(Poäng);Console.WriteLine("Poäng");
+Console.WriteLine($"Du har {Poäng} Poäng");
 Console.WriteLine("Hur gammal är Jag");
 Console.WriteLine("1)16 2)17 3)18");
 string ans2 = Console.ReadLine();
@@ -34,7 +34,7 @@ else if (ans2 == "3")
    Console.WriteLine("Fel svar");
 }
 
-Console.WriteLine("Du har"); Console.WriteLine(Poäng);Console.WriteLine("Poäng");
+Console.WriteLine($"Du har {Poäng} Poäng");
 Console.WriteLine("När är jag född");
 Console.WriteLine("1)12 Maj 2)12 September 3)12 Feburary");
 string ans3 = Console.ReadLine();
@@ -53,22 +53,22 @@ Console.WriteLine("Bra jobbat");
 Poäng = Poäng +10;
 }
 Console.WriteLine("Så här bra gjorde du");
-Console.WriteLine("Du fick totalt"); Console.WriteLine(Poäng);Console.WriteLine("Poäng");
+Console.WriteLine($"Du har {Poäng} Poäng");
 if (Poäng == 30)
 {
  Console.WriteLine ("Alla rätt Bra jobbat");
 }
 else if (Poäng == 20)
 {
-   Console.WriteLine ("A du är ändå helt okej");
+   Console.WriteLine ("men a du är ändå helt okej");
 }
 else if (Poäng == 10) 
 {
-Console.WriteLine("Katastrofalt");
+Console.WriteLine("Katastrofalt gjort");
 }
 else if (Poäng == 0) 
 {
-Console.WriteLine("Katastrofalt");
+Console.WriteLine("Det här är Katastrofalt");
 }
 
 Console.ReadLine();
